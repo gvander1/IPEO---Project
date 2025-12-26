a79cfdbe5c49309ae2a0a33ea37870c5a4951dfd
Maxime Steiner <msteiner@izar1.izar.cluster>
2025-12-26 23:17:14 +0100

    )

--- run_test-12-12-2025.py
+++ run_test-12-12-2025.py
@@ -30,7 +30,7 @@ transforms = T.Compose([
     T.Normalize(mean, std)
 ])
 
-train=LCAmazon(root="DATA", modality="s2", split="train"
+train=LCAmazon(root="DATA", modality="s2", split="train")
 val=LCAmazon(root="DATA", modality="s2", split="val")
 test=LCAmazon(root="DATA", modality="s2", split="test")
 print(f"train of length {len(train)}, val of length {len(val)}, test of length {len(test)}")
